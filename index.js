9e87a133541bf52c60fe8c79cf8f68d0e8bd9301
AL JABER <aljaberlax@gmail.com>
2022-05-07 17:40:32 +0600

    app.get product

--- index.js
+++ index.js
@@ -1,33 +1,48 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const port = process.env.PORT || 5000;
-
+require('dotenv').config();
 const app = express();
+const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+
 
 // middleware
 app.use(cors());
 app.use(express.json());
 
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.PASS}@cluster0.qx5zh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+
+const uri = "mongodb+srv://DB_bikeBd_user:TujpQ3lICRFRqa2O@cluster0.xrerq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  console.log('db connected')
-  // perform actions on the collection object
-  client.close();
+    
+    const productCollection = client.db('Bike').collection('product');
+    // perform actions on the collection object
+    app.get('/product', async (req, res) => {
+
+        const quary = {}
+        const cursor = productCollection.find(quary)
+        const products = await cursor.toArray();
+        res.send(products)
+    });
+    
+    //post 
+
+    app.post('/product', async (req, res) => {
+        const newProduct = req.body;
+        const result = await productCollection.insertOne(newProduct)
+        res.send(result)
+    })
+    
+    console.log('connected to bike db')
 });
+
 
 
 app.get('/', (req, res) => {
     res.send('Running Bike BD server');
 });
-
-
-
-
-
 
 app.listen(port, () => {
     console.log('Listening to port', port);
